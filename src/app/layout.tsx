3259df4df07db5371200181f15da248b00560943
LJS0000 <leejisu2281@gmail.com>
2024-10-27 01:48:27 +0900

    💄 UI_글꼴 적용-Pretendard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next'
 import '../styles/globals.css'
+import localFont from 'next/font/local'
 
 export const metadata: Metadata = {
   title: 'KKUN-KKUN',
   description: '끈끈한 사이가 되는 방법',
 }
+
+const pretendard = localFont({
+  src: '../styles/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
 
 export default function RootLayout({
   children,
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className='antialiased'>{children}</body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   )
 }
